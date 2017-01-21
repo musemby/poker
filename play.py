3ac71001091a13ad348b4e8d0b8312ad0bf3708e
Author Name <musembinzioki@gmail.com>
2017-01-21 16:20:20 +0300

    Achieved a valid 54 and 52 set pack

--- play.py
+++ play.py
@@ -1,30 +1,60 @@
 import random 
 
 
-ranks = ['ace', 'king', 'queen', 'jack'] + [str(num) for num in range(2, 10)]
+ranks = ['ace', 'king', 'queen', 'jack'] + [str(num) for num in range(2, 11)]
 suits = ['clubs', 'diamonds', 'hearts', 'spades']
 
 class Card():
     """Definition of a card"""
-    def __init__(self, rank, suit, joker=False):
-        self.rank = str(rank)
-        self.suit = str(suit)
+    def __init__(self, rank=None, suit=None, joker=False):
+        self.rank = rank
+        self.suit = suit
         self.joker = joker
 
         if self.joker and (self.suit or self.rank):
             raise Exception('A joker card cannot have a suit or rank')
 
-        if self.rank not in ranks:
+        if self.rank and str(self.rank) not in ranks:
             raise Exception('The rank provided is invalid')
 
-        if self.suit not in suits:
+        if self.rank and str(self.suit) not in suits:
             raise Exception('The suit provided is invalid')
+
+    def __repr__(self):
+        return self.rank + ' of ' + self.suit if self.rank and self.suit else 'Poker'
 
 
 class Pack():
     """Definition of a pack of cards"""
-    cards = []
-    def __init__(self, type):
-        for rank in ranks:
-            for suit in suits:
-                self.cards.append(Card(rank, suit))
+    def __init__(self, size):
+        self.size = int(size)
+        self.cards = []
+
+        if self.size not in [52, 54]:
+            raise Exception('Only 52 and 54 card decks are accepted')
+
+        if len(self.cards) < self.size:
+            for rank in ranks:
+                for suit in suits:
+                    self.add_to_pack(Card(rank=rank, suit=suit))
+            if self.size == 54:
+                self.add_to_pack(Card(joker=True), quantity=2)
+
+    def add_to_pack(self, card, quantity=1):
+        if not isinstance(card, Card):
+            raise Exception('You can only add a card to the pack')
+        self.cards.append(card)
+
+        if card.joker:
+            for _ in xrange(quantity-1):
+               self.cards.append(card)
+
+
+
+    def __repr__(self):
+        return '{0} Pack with {1} cards'.format(self.size, len(self.cards)) 
+
+pak=Pack(54)
+
+import pdb
+pdb.set_trace()
